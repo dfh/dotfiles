53997437af986e103071393d23693859c240a0d5
David <david@jacoby>
2022-10-04 19:14:54 +0200

    Zsh configuration files.

---
+++ zsh/install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "Backing up existing files..."
+echo
+
+#
+# --no-clobber makes cp not overwrite existing target.
+#
+# --no-target-directory makes cp not create a subdirectory within the target
+# directory if it already exists.
+#
+# The "| true" thing avoids exiting with error if target file already exists.
+#
+cp -v --no-clobber ~/.zshenv ~/.zshenv.orig || true
+cp -v --no-clobber ~/.config/zsh/.zshrc ~/.config/zsh/.zshrc.orig || true
+cp -v --no-clobber ~/.config/zsh/.zimrc ~/.config/zsh/.zimrc.orig || true
+cp -v --no-clobber --recursive --no-target-directory ~/.config/zsh/asciiship-djf ~/.config/zsh/asciiship-djf.orig || true
+
+echo
+echo "Copying new files in place..."
+echo
+
+cp -v zshenv ~/.zshenv
+cp -v zshrc ~/.config/zsh/.zshrc
+cp -v zimrc ~/.config/zsh/.zimrc
+cp -v --recursive --no-target-directory asciiship-djf ~/.config/zsh/asciiship-djf
+
+echo
+echo "All done."
---
+++ zsh/uninstall.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "Restoring original files:"
+echo
+
+#
+# --force makes mv overwrite target without asking.
+#
+# the "| true" thing makes mv not exit with error if target does not exist.
+#
+mv -v ~/.zshenv.orig ~/.zshenv || true
+mv -v ~/.config/zsh/.zshrc.orig ~/.config/zsh/.zshrc || true
+mv -v ~/.config/zsh/.zimrc.orig ~/.config/zsh/.zimrc || true
+mv -v --force ~/.config/zsh/asciiship-djf.orig/ ~/.config/zsh/asciiship-djf || true
+
+echo
+echo "All done."
