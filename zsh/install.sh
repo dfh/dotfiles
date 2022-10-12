e0cc119be20dbfb0ebf6a841e3ef5b8f5a40e183
David <david@jacoby>
2022-10-12 19:55:36 +0200

    Zsh: Improved install and uninstall scripts.
    Also loads secrets from ~/.config/secrets.sh.

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,30 +1,50 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Backing up existing files..."
+echo "Backing up existing zshenv and zshrc files (unless backups already exist)..."
 echo
 
-#
-# --no-clobber makes cp not overwrite existing target.
-#
-# --no-target-directory makes cp not create a subdirectory within the target
-# directory if it already exists.
-#
-# The "|| true" thing avoids exiting with error if target file already exists.
-#
-cp -v --no-clobber ~/.zshenv ~/.zshenv.orig || true
-cp -v --no-clobber ~/.config/zsh/.zshrc ~/.config/zsh/.zshrc.orig || true
-cp -v --no-clobber ~/.config/zsh/.zimrc ~/.config/zsh/.zimrc.orig || true
-cp -v --no-clobber --recursive --no-target-directory ~/.config/zsh/asciiship-djf ~/.config/zsh/asciiship-djf.orig || true
+backed_up=false
+if [[ -f ~/.zshenv && ! -f ~/.zshenv.orig ]]; then
+   cp -v ~/.zshenv ~/.zshenv.orig
+   backed_up=true
+fi
+if [[ -f ~/.config/zsh/.zshrc && ! -f ~/.config/zsh/.zshrc.orig ]]; then
+   cp -v ~/.config/zsh/.zshrc ~/.config/zsh/.zshrc.orig
+   backed_up=true
+fi
+if [ "$backed_up" = false ]; then
+    echo "No files to back up."
+fi
 
 echo
 echo "Copying new files in place..."
 echo
 
+#
+# The "|| true" makes the script not exit with error if files are not found.
+#
+
+rm -v ~/.zshenv || true
+rm -v ~/.config/zsh/.zshrc || true
+rm -v ~/.config/zsh/.zimrc || true
+rm -vrf ~/.config/zsh/asciiship-djf || true
+
+echo
+
 cp -v zshenv ~/.zshenv
 cp -v zshrc ~/.config/zsh/.zshrc
 cp -v zimrc ~/.config/zsh/.zimrc
-cp -v --recursive --no-target-directory asciiship-djf ~/.config/zsh/asciiship-djf
+cp -v --recursive asciiship-djf ~/.config/zsh/asciiship-djf
+
+echo
+if [[ ! -f ~/.config/secrets.sh ]]; then
+    echo "~/.config/secrets.sh does not exist, touching and chmod:ing it."
+    touch ~/.config/secrets.sh
+    chmod -v 600 ~/.config/secrets.sh
+else
+    echo "~/.config/secrets.sh already exists, not touching it."
+fi
 
 echo
 echo "All done."
--- zsh/uninstall.sh
+++ zsh/uninstall.sh
@@ -1,18 +1,38 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Restoring original files:"
+echo
+echo "Removing installed files..."
 echo
 
-#
-# --force makes mv overwrite target without asking.
-#
-# the "|| true" thing makes mv not exit with error if target does not exist.
-#
-mv -v ~/.zshenv.orig ~/.zshenv || true
-mv -v ~/.config/zsh/.zshrc.orig ~/.config/zsh/.zshrc || true
-mv -v ~/.config/zsh/.zimrc.orig ~/.config/zsh/.zimrc || true
-mv -v --force ~/.config/zsh/asciiship-djf.orig/ ~/.config/zsh/asciiship-djf || true
+rm -v ~/.zshenv || true
+rm -v ~/.config/zsh/.zshrc || true
+rm -v ~/.config/zsh/.zimrc || true
+rm -vrf ~/.config/zsh/asciiship-djf || true
+
+echo
+echo "Restoring original zshenv and zshrc if backups found..."
+echo
+
+#((restored = 1)) || true
+restored=false
+if [[ -f ~/.zshenv.orig ]]; then
+    mv -v ~/.zshenv.orig ~/.zshenv
+    restored=true
+    #((restored++))
+fi
+if [[ -f ~/.config/zsh/.zshrc.orig ]]; then
+    mv -v ~/.config/zsh/.zshrc.orig ~/.config/zsh/.zshrc
+#    (( restored++))
+    restored=true
+fi
+
+if [[ "$restored" = false ]]; then
+    echo "No backups found."
+fi
 
 echo
 echo "All done."
+echo
+
+echo "NOTE! ${HOME}/.config/secrets.sh must be removed manually."
