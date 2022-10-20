b0c9edf75238c8afe18181238f120f7d11fccfba
David Högberg <david@hgbrg.se>
2022-10-20 16:28:36 +0200

    Git: Set user.name and user.email.

--- git/install.sh
+++ git/install.sh
@@ -22,9 +22,21 @@ echo "  git config --global core.excludesfile ~/.config/git/gitignore"
 git config --global core.excludesfile ~/.config/git/gitignore
 
 echo
-echo "All done. New global gitignore installed to: ~/.config/git/gitignore"
+echo "New global gitignore installed to: ~/.config/git/gitignore"
+echo
+echo "Setting name and email..."
+echo
+
+echo "  git config --global user.name \"David Högberg\""
+git config --global user.name "David Högberg"
+echo "  git config --global user.name \"david@hgbrg.se\""
+git config --global user.email "david@hgbrg.se"
+
+echo
+echo "All done."
 
 if [[ -f $HOME/.gitignore ]]; then
     echo
     echo "NOTICE: Found existing ~/.gitignore. It won't be used anymore."
 fi
+
